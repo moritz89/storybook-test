be326559f1d08a0fb6a9b0e88c24d43ada25b27f
Moritz Ulmer <moritz.ulmer@posteo.de>
2021-10-20 07:24:25 +0200

    Add task lists
    Why:
    
    - Allow lists of tasks to be created
    
    This change addresses the need by:
    
    - Adding a task list component
    - Adding tests
    - Adding stories

--- src/jest-config/setup.ts
+++ src/jest-config/setup.ts
@@ -1,4 +1,4 @@
-import 'jest-preset-angular';
+import 'jest-preset-angular/setup-jest';
 import './__mocks__/globalMocks';
 
 Object.defineProperty(global, 'Promise', { writable: false, value: global.Promise });
